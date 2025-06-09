ac29487b0c91b6062ab98a9e02f17ca25ee7336a
Charlie <charlieang04@gmail.>
2025-06-09 21:44:28 +0700

    layout page

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: [
+        './resources/**/*.blade.php',
+        './resources/**/*.js',
+        './resources/**/*.vue',
+    ],
+    darkMode: 'class',
+    theme: {
+        extend: {},
+    },
+    plugins: [
+        require('preline/plugin'),
+    ],
+}
